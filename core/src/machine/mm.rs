bb3860ba4901aa2328268a564f5977a3130efab4
Johan M. von Behren <johan@vonbehren.eu>
2018-08-21 15:34:19 +0200

    Add io register to `Machine` and extend memory mapping

--- core/src/machine/mm.rs
+++ core/src/machine/mm.rs
@@ -13,17 +13,17 @@ impl Machine {
         // TODO :(
         match addr.get() {
             // ROM mounted switch
-            0x0000..0x0100 if self.bios_mounted => self.bios[addr],
+            0x0000..0x0100 if self.bios_mounted() => self.bios[addr],
 
-            0x0000..0x8000 => unimplemented!(), // Cartridge
-            0x8000..0xA000 => unimplemented!(), // vram
+            0x0000..0x8000 => self.cartridge.load_byte(addr), // Cartridge
+            0x8000..0xA000 => self.vram[addr - 0x8000], // vram
             0xA000..0xC000 => unimplemented!(), // exram
             0xC000..0xE000 => self.wram[addr - 0xC000], // wram
-            0xE000..0xFE00 => self.wram[addr - 0xC000 - 0x2000], // wram echo
+            0xE000..0xFE00 => self.wram[addr - 0xE000], // wram echo
             0xFE00..0xFEA0 => unimplemented!(), // oam
             0xFEA0..0xFF00 => unimplemented!(), // not usable (random ram, maybe use as rng???)
-            0xFF00..0xFF80 => unimplemented!(), // IO registers
-            0xFF80..0xFFFF => unimplemented!(), // hram
+            0xFF00..0xFF80 => self.io[addr - 0xFF00], // IO registers
+            0xFF80..0xFFFF => self.hram[addr - 0xFF80], // hram
             0xFFFF => self.ie, // ie
             _ => unreachable!(),
         }
@@ -33,17 +33,23 @@ impl Machine {
     pub(crate) fn store_byte(&mut self, addr: Word, byte: Byte) {
         match addr.get() {
             // ROM mounted switch
-            0x0000..0x0100 if self.bios_mounted => warn!("Wrote to BIOS ROM!"),
+            0x0000..0x0100 if self.bios_mounted() => warn!("Wrote to BIOS ROM!"),
 
-            0x0000..0x8000 => unimplemented!(), // Cartridge
+            0x0000..0x8000 => warn!("Wrote to cartridge!"), // Cartridge
             0x8000..0xA000 => self.vram[addr - 0x8000] = byte, // vram
             0xA000..0xC000 => unimplemented!(), // exram
             0xC000..0xE000 => self.wram[addr - 0xC000] = byte, // wram
             0xE000..0xFE00 => self.wram[addr - 0xE000] = byte, // wram echo
             0xFE00..0xFEA0 => unimplemented!(), // oam
             0xFEA0..0xFF00 => unimplemented!(), // not usable (random ram, maybe use as rng???)
-            0xFF00..0xFF80 => unimplemented!(), // IO registers
-            0xFF80..0xFFFF => unimplemented!(), // hram
+
+            // Register with flag for mounting/unmounting the BIOS (this is an IO register). To
+            // this register may only be written, if the BIOS is mounted. When the BIOS is
+            // unmounted, the write access is denied. We assume the Gameboy hardware does the same.
+            0xFF50 if !self.bios_mounted() => warn!("Tried to re-mount BIOS!"),
+
+            0xFF00..0xFF80 => self.io[addr - 0xFF00] = byte, // IO registers
+            0xFF80..0xFFFF => self.hram[addr - 0xFF80] = byte, // hram
             0xFFFF => unimplemented!(), // ie
             _ => unreachable!(),
         }
