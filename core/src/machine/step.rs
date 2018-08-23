2afbef7b93423cefe20b806b3eb6e501a62b7da6
Johan M. von Behren <johan@vonbehren.eu>
2018-08-23 22:55:12 +0200

    Add implementation for further instructions

--- core/src/machine/step.rs
+++ core/src/machine/step.rs
@@ -93,32 +93,69 @@ impl Machine {
         // Normal method stuff starts here
         let action_taken = match op_code.get() {
             // ========== LD ==========
-            opcode!("LD B, d8") => {
-                self.cpu.b = arg_byte;
+            opcode!("LD B, d8") => ld_d8!(self.cpu.b),
+            opcode!("LD C, d8") => ld_d8!(self.cpu.c),
+            opcode!("LD A, d8") => ld_d8!(self.cpu.a),
+            opcode!("LD E, d8") => ld_d8!(self.cpu.e),
+            opcode!("LD L, d8") => ld_d8!(self.cpu.l),
+            opcode!("LD D, d8") => ld_d8!(self.cpu.d),
+            opcode!("LD H, d8") => ld_d8!(self.cpu.h),
 
-                false
-            }
-            opcode!("LD C, d8") => {
-                self.cpu.c = arg_byte;
+            opcode!("LD B, B") => ld!(self.cpu.b, self.cpu.b),
+            opcode!("LD B, C") => ld!(self.cpu.b, self.cpu.c),
+            opcode!("LD B, D") => ld!(self.cpu.b, self.cpu.d),
+            opcode!("LD B, E") => ld!(self.cpu.b, self.cpu.e),
+            opcode!("LD B, H") => ld!(self.cpu.b, self.cpu.h),
+            opcode!("LD B, L") => ld!(self.cpu.b, self.cpu.l),
+            opcode!("LD B, A") => ld!(self.cpu.b, self.cpu.a),
 
-                false
-            }
-            opcode!("LD A, d8") => {
-                self.cpu.a = arg_byte;
+            opcode!("LD C, B") => ld!(self.cpu.c, self.cpu.b),
+            opcode!("LD C, C") => ld!(self.cpu.c, self.cpu.c),
+            opcode!("LD C, D") => ld!(self.cpu.c, self.cpu.d),
+            opcode!("LD C, E") => ld!(self.cpu.c, self.cpu.e),
+            opcode!("LD C, H") => ld!(self.cpu.c, self.cpu.h),
+            opcode!("LD C, L") => ld!(self.cpu.c, self.cpu.l),
+            opcode!("LD C, A") => ld!(self.cpu.c, self.cpu.a),
 
-                false
-            }
+            opcode!("LD D, B") => ld!(self.cpu.d, self.cpu.b),
+            opcode!("LD D, C") => ld!(self.cpu.d, self.cpu.c),
+            opcode!("LD D, D") => ld!(self.cpu.d, self.cpu.d),
+            opcode!("LD D, E") => ld!(self.cpu.d, self.cpu.e),
+            opcode!("LD D, H") => ld!(self.cpu.d, self.cpu.h),
+            opcode!("LD D, L") => ld!(self.cpu.d, self.cpu.l),
+            opcode!("LD D, A") => ld!(self.cpu.d, self.cpu.a),
 
-            opcode!("LD C, A") => {
-                self.cpu.c = self.cpu.a;
+            opcode!("LD E, B") => ld!(self.cpu.e, self.cpu.b),
+            opcode!("LD E, C") => ld!(self.cpu.e, self.cpu.c),
+            opcode!("LD E, D") => ld!(self.cpu.e, self.cpu.d),
+            opcode!("LD E, E") => ld!(self.cpu.e, self.cpu.e),
+            opcode!("LD E, H") => ld!(self.cpu.e, self.cpu.h),
+            opcode!("LD E, L") => ld!(self.cpu.e, self.cpu.l),
+            opcode!("LD E, A") => ld!(self.cpu.e, self.cpu.a),
 
-                false
-            }
-            opcode!("LD A, E") => {
-                self.cpu.a = self.cpu.e;
+            opcode!("LD H, B") => ld!(self.cpu.h, self.cpu.b),
+            opcode!("LD H, C") => ld!(self.cpu.h, self.cpu.c),
+            opcode!("LD H, D") => ld!(self.cpu.h, self.cpu.d),
+            opcode!("LD H, E") => ld!(self.cpu.h, self.cpu.e),
+            opcode!("LD H, H") => ld!(self.cpu.h, self.cpu.h),
+            opcode!("LD H, L") => ld!(self.cpu.h, self.cpu.l),
+            opcode!("LD H, A") => ld!(self.cpu.h, self.cpu.a),
 
-                false
-            }
+            opcode!("LD L, B") => ld!(self.cpu.l, self.cpu.b),
+            opcode!("LD L, C") => ld!(self.cpu.l, self.cpu.c),
+            opcode!("LD L, D") => ld!(self.cpu.l, self.cpu.d),
+            opcode!("LD L, E") => ld!(self.cpu.l, self.cpu.e),
+            opcode!("LD L, H") => ld!(self.cpu.l, self.cpu.h),
+            opcode!("LD L, L") => ld!(self.cpu.l, self.cpu.l),
+            opcode!("LD L, A") => ld!(self.cpu.l, self.cpu.a),
+
+            opcode!("LD A, B") => ld!(self.cpu.a, self.cpu.b),
+            opcode!("LD A, C") => ld!(self.cpu.a, self.cpu.c),
+            opcode!("LD A, D") => ld!(self.cpu.a, self.cpu.d),
+            opcode!("LD A, E") => ld!(self.cpu.a, self.cpu.e),
+            opcode!("LD A, H") => ld!(self.cpu.a, self.cpu.h),
+            opcode!("LD A, L") => ld!(self.cpu.a, self.cpu.l),
+            opcode!("LD A, A") => ld!(self.cpu.a, self.cpu.a),
 
             opcode!("LD A, (DE)") => {
                 let val = self.load_byte(self.cpu.de());
@@ -183,19 +220,31 @@ impl Machine {
 
             // ========== DEC ==========
             opcode!("DEC B") => dec!(self.cpu.b),
+            opcode!("DEC D") => dec!(self.cpu.d),
+            opcode!("DEC H") => dec!(self.cpu.h),
+            opcode!("DEC C") => dec!(self.cpu.c),
+            opcode!("DEC E") => dec!(self.cpu.e),
+            opcode!("DEC L") => dec!(self.cpu.l),
             opcode!("DEC A") => dec!(self.cpu.a),
 
+            opcode!("DEC BC") => no_branch!(self.cpu.set_bc(self.cpu.bc() - 1u16)),
+            opcode!("DEC DE") => no_branch!(self.cpu.set_de(self.cpu.de() - 1u16)),
+            opcode!("DEC HL") => no_branch!(self.cpu.set_hl(self.cpu.hl() - 1u16)),
+            opcode!("DEC SP") => no_branch!(self.cpu.sp -= 1u16),
+
             // ========== INC ==========
-            opcode!("INC DE") => {
-                self.cpu.set_de(self.cpu.de() + 1u16);
+            opcode!("INC B") => inc!(self.cpu.b),
+            opcode!("INC D") => inc!(self.cpu.d),
+            opcode!("INC H") => inc!(self.cpu.h),
+            opcode!("INC C") => inc!(self.cpu.c),
+            opcode!("INC E") => inc!(self.cpu.e),
+            opcode!("INC L") => inc!(self.cpu.l),
+            opcode!("INC A") => inc!(self.cpu.a),
 
-                false
-            }
-            opcode!("INC HL") => {
-                self.cpu.set_hl(self.cpu.hl() + 1u16);
-
-                false
-            }
+            opcode!("INC BC") => no_branch!(self.cpu.set_bc(self.cpu.bc() + 1u16)),
+            opcode!("INC DE") => no_branch!(self.cpu.set_de(self.cpu.de() + 1u16)),
+            opcode!("INC HL") => no_branch!(self.cpu.set_hl(self.cpu.hl() + 1u16)),
+            opcode!("INC SP") => no_branch!(self.cpu.sp += 1u16),
 
             // ========== SUB ==========
             opcode!("SUB L") => {
@@ -225,8 +274,22 @@ impl Machine {
             }
 
             // ========== JR ==========
+            opcode!("JR r8") => {
+                self.cpu.pc += arg_byte.get() as i8;
+
+                false
+            }
             opcode!("JR NZ, r8") => {
                 if !self.cpu.zero() {
+                    self.cpu.pc += arg_byte.get() as i8;
+
+                    true
+                } else {
+                    false
+                }
+            }
+            opcode!("JR Z, r8") => {
+                if self.cpu.zero() {
                     self.cpu.pc += arg_byte.get() as i8;
 
                     true
