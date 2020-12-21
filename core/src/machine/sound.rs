2d7f8f83475d6e48bde8b30e9af44a1e722b218f
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2020-12-21 12:12:04 +0100

    Implement basic frame sequencer and length counter for wave channel

--- core/src/machine/sound.rs
+++ core/src/machine/sound.rs
@@ -1,12 +1,14 @@
-use crate::{
-    primitives::{Byte, Memory, Word},
-};
+use crate::primitives::{Byte, Memory, Word};
 
 
 // TODO: Because of the lack of information some assumptions has been made which need proove:
 // - Not readable registers/bits return 0xFF or 1 when read.
 
 /// Manages the sound. This is mapped to `0xFF10..0xFF40` in the Memory.
+///
+/// Unused bits read 1 and writes are ignored. In our implementation we maintain
+/// that unused bits in our stored `Byte`s are indeed 1. So on read, we just
+/// return them; on write we `|` the input value.
 pub(crate) struct SoundController {
     channel1_sweep: Byte,
     channel1_length: Byte,
@@ -29,6 +31,13 @@ pub(crate) struct SoundController {
     sound_on_off: Byte,
 
     wave: WaveChannel,
+
+    /// A counter used to generate a 512Hz clock. This is used to control length
+    /// (256Hz), volume (64Hz) and sweep (128Hz) counters of the sound channels.
+    /// This particular counter is incremented each machine cycle (1_048_576
+    /// Hz). As the slowest clock we want to generate is 64Hz, this counter
+    /// wraps at `1_048_576 / 64 = 16_384`.
+    frame_sequencer: u32,
 }
 
 impl SoundController {
@@ -52,6 +61,7 @@ impl SoundController {
             sound_on_off: Byte::zero(),
 
             wave: WaveChannel::new(),
+            frame_sequencer: 0,
         }
     }
 
@@ -126,7 +136,31 @@ impl SoundController {
     /// Executes one machine cycle (1,048,576 Hz) of the sound system. Returns
     /// the current sound output.
     pub(crate) fn step(&mut self) {
-        // self.counter = (self.counter + 1) % 2u16.pow(13);
+        self.frame_sequencer += 1;
+
+        // This is the 512Hz clock (1_048_576 / 512 = 2048).
+        if self.frame_sequencer % 2048 == 0 {
+            let rem = self.frame_sequencer % 2048;
+
+            // 256Hz length clock.
+            if rem % 2 == 0 {
+                self.wave.clock_length();
+            }
+
+            // 128Hz sweep clock.
+            if rem == 2 || rem == 6 {
+            }
+
+            // 64Hz volume envelop clock.
+            if rem == 7 {
+            }
+
+            // Wrap frame sequencer.
+            if rem == 8 {
+                self.frame_sequencer = 0;
+            }
+        }
+
         self.wave.step();
     }
 
@@ -136,9 +170,37 @@ impl SoundController {
     }
 }
 
-// TODO:
-// - length
-// - reading and writing wave data when enabled
+/// The wave channel.
+///
+/// Things not implemented (and maybe never will, because weird):
+/// - length
+/// - "When triggering the wave channel, the first sample to play is the
+///   previous one still in the high nibble of the sample buffer, and the next
+///   sample is the second nibble from the wave table. This is because it
+///   doesn't load the first byte on trigger like it 'should'. The first nibble
+///   from the wave table is thus not played until the waveform loops."
+/// - "Triggering the wave channel on the DMG while it reads a sample byte will
+///   alter the first four bytes of wave RAM. If the channel was reading one of
+///   the first four bytes, the only first byte will be rewritten with the byte
+///   being read. If the channel was reading one of the later 12 bytes, the
+///   first FOUR bytes of wave RAM will be rewritten with the four aligned bytes
+///   that the read was from (bytes 4-7, 8-11, or 12-15); for example if it were
+///   reading byte 9 when it was retriggered, the first four bytes would be
+///   rewritten with the contents of bytes 8-11. To avoid this corruption you
+///   should stop the wave by writing 0 then $80 to NR30 before triggering it
+///   again. The game Duck Tales encounters this issue part way through most
+///   songs."
+/// - Initial wave channel data after powering on. Different between Gameboy
+///   models.
+/// - "CH3 output level control does not, in fact, alter the output level. It
+///   shifts the digital value CH3 is outputting (read below), not the analog
+///   value." -> I am not sure what this means exactly. Also: "That value is
+///   digital, and can range between 0 and 0xF. This is then fed to a DAC, which
+///   maps this to an analog value; 7 maps to the lowest (negative) voltage, 0
+///   to the highest (positive) one.". So either the digital signal is signed
+///   and that works out somehow, OR the value is indeed shifted and we rely on
+///   the high-pass filter to make sure the DC offset of "25% volume" is
+///   removed.
 struct WaveChannel {
     enable: Byte,       // FF1A  E111_1111
     length: Byte,       // FF1B
@@ -153,6 +215,9 @@ struct WaveChannel {
     /// Internal timer which counts down. This value is reloaded with
     /// `(2048 - self.freq()) * 2`.
     timer: u16,
+
+    // This is an internal counter which can be loaded by writing `length`.
+    length_counter: u16,
 }
 
 impl WaveChannel {
@@ -166,22 +231,24 @@ impl WaveChannel {
             wave_table: Memory::zeroed(Word::new(0x10)),
             position: 0,
             timer: 0,
+            length_counter: 0,
         }
     }
 
-    fn freq(&self) -> u16 {
-        self.freq_lo.get() as u16 + ((self.control_freq.get() as u16 & 0b111) << 8)
-    }
-
-    fn timer_reset_value(&self) -> u16 {
+    fn reset_timer(&mut self) {
         // The "correct" counter value is `(2048 - freq) * 2`, but that's for
         // when the timer is decremented with 4Mhz. We only decrement with 1Mhz,
         // so we divide that by 4.
-        (2048 - self.freq()) / 2
+        let freq = self.freq_lo.get() as u16 + ((self.control_freq.get() as u16 & 0b111) << 8);
+        self.timer = (2048 - freq) / 2;
     }
 
-    fn enabled(&self) -> bool {
+    fn dac_enabled(&self) -> bool {
         self.enable.get() & 0b1000_0000 != 0
+    }
+
+    fn is_length_enabled(&self) -> bool {
+        self.control_freq.get() & 0b0100_0000 != 0
     }
 
     pub(crate) fn load_byte(&self, addr: Word) -> Byte {
@@ -191,16 +258,28 @@ impl WaveChannel {
             0x0C => self.volume,
             0x0D => self.freq_lo,
             0x0E => self.control_freq,
-            0x20..=0x2F => self.wave_table[addr - 0x20],
+            0x20..=0x2F => {
+                if self.dac_enabled() {
+                    // This behavior is very weird and different between
+                    // different Gameboy models. Returning FF is what some do,
+                    // and as no game should be relying on weird behavior, we
+                    // just always return FF.
+                    Byte::new(0xFF)
+                } else {
+                    self.wave_table[addr - 0x20]
+                }
+            }
             _ => unreachable!(),
         }
     }
 
-
     fn store_byte(&mut self, addr: Word, byte: Byte) {
         match addr.get() {
             0x0A => self.enable = byte.mask_or(0b1000_0000),
-            0x0B => self.length = byte,
+            0x0B => {
+                self.length = byte;
+                self.length_counter = 256 - byte.get() as u16;
+            }
             0x0C => self.volume = byte.mask_or(0b0110_0000),
             0x0D => self.freq_lo = byte,
             0x0E => {
@@ -209,7 +288,15 @@ impl WaveChannel {
                     self.trigger();
                 }
             }
-            0x20..=0x2F => self.wave_table[addr - 0x20] = byte,
+            0x20..=0x2F => {
+                // The behavior when the channel is activated is very weird and
+                // different between different Gameboy models. Ignoring the
+                // write is what some do, and as no game should be relying on
+                // weird behavior, we ignore it.
+                if !self.dac_enabled() {
+                    self.wave_table[addr - 0x20] = byte;
+                }
+            }
             _ => unreachable!(),
         }
     }
@@ -217,20 +304,26 @@ impl WaveChannel {
     fn trigger(&mut self) {
         // TODO: "If length counter is zero, it is set to 64 (256 for wave channel)."
         self.position = 0;
-        self.timer = self.timer_reset_value();
+        self.reset_timer();
+    }
+
+    fn clock_length(&mut self) {
+        if self.is_length_enabled() && self.length_counter > 0 {
+            self.length_counter -= 1;
+        }
     }
 
     fn step(&mut self) {
         if self.timer > 0 {
             self.timer -= 1;
         } else {
-            self.timer = self.timer_reset_value();
+            self.reset_timer();
             self.position = (self.position + 1) % 32;
         }
     }
 
     fn output(&self) -> f32 {
-        if !self.enabled() {
+        if !self.dac_enabled() || (self.is_length_enabled() && self.length_counter == 0) {
             return 0.0;
         }
 
@@ -241,6 +334,7 @@ impl WaveChannel {
             b & 0xF
         };
 
+        // This is probably wrong, see type docs.
         let volume = match (self.volume.get() & 0b0110_0000) >> 5 {
             0 => 0.0,
             1 => 1.0,
@@ -255,6 +349,11 @@ impl WaveChannel {
 
 /// Mimics the digital analog converted that converts a 4 bit number into an
 /// analog signal.
+///
+/// This is probably still not correct: "That value is digital, and can range
+/// between 0 and 0xF. This is then fed to a DAC, which maps this to an analog
+/// value; 7 maps to the lowest (negative) voltage, 0 to the highest (positive)
+/// one." (This quote is strange tho, what happens with 8-F?)
 fn dac(input: u8) -> f32 {
     (input as f32 / 7.5) - 1.0
 }
